aea3d8e62c0f7eb8df9187da66a6de88f8ea495f
Ubbemir <ubbermir@example.com>
2025-02-23 00:47:07 +0100

    Added arguments and a proper main program

---
+++ main.py
@@ -0,0 +1,34 @@
+import formatters
+import argparse
+
+from lark import Lark
+
+def main():
+    parser = argparse.ArgumentParser(
+                    prog="Source Engine CFG Formatter",
+                    description="Formats CFG files from source engine games.")
+    
+    parser.add_argument("input_file", help="input file to be formatted")
+    parser.add_argument("-m", "--minify", help="minify instead of beutify",
+                    action="store_true")
+
+    args = parser.parse_args()
+
+    with open("source_cfg.lark", "r") as file:
+        cfg_grammar = file.read()
+    cfg_parser = Lark(cfg_grammar, parser='earley')
+
+
+    with open(args.input_file, "r") as file:
+        input_content = file.read()
+
+    result = ""
+    if args.minify:
+        result = formatters.minify_cfg(cfg_parser.parse(input_content))
+    else:
+        result = formatters.prettify_cfg(cfg_parser.parse(input_content))
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
