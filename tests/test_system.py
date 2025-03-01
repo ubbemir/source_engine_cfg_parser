5f8e7e5c2983d217480937e145a5922ea73eeb85
Ubbemir <ubbermir@example.com>
2025-03-01 16:39:41 +0100

    Another minification test

--- tests/test_system.py
+++ tests/test_system.py
@@ -10,11 +10,31 @@ def get_parser():
     return Lark(cfg_grammar, parser="earley")
 
 class TestFormatter(unittest.TestCase):
-    def test_minify_1(self):
+    def test_minify_simple(self):
         test_input = """
             bot_kick
             sv_cheats 1
             mp_restartgame 1
+        """
+        minified = formatters.minify_cfg(get_parser().parse(test_input))
+        self.assertEqual(minified, "bot_kick;sv_cheats 1;mp_restartgame 1")
+
+    def test_minify_whitespaces(self):
+        test_input = """
+
+
+            
+            bot_kick   
+
+               
+            sv_cheats 1  
+             
+            
+            mp_restartgame 1;   
+
+
+
+
         """
         minified = formatters.minify_cfg(get_parser().parse(test_input))
         self.assertEqual(minified, "bot_kick;sv_cheats 1;mp_restartgame 1")
