e2a808c7646aaa7d07701403a3c3794a7d904e4c
Ubbemir <ubbermir@example.com>
2025-03-01 16:47:56 +0100

    Added complex minification test

--- tests/test_system.py
+++ tests/test_system.py
@@ -39,6 +39,14 @@ class TestFormatter(unittest.TestCase):
         minified = formatters.minify_cfg(get_parser().parse(test_input))
         self.assertEqual(minified, "bot_kick;sv_cheats 1;mp_restartgame 1")
 
+    def test_minify_complex(self):
+        test_input = """
+            bind "mouse4" "+jump;+right";;
+            sv_cheats 1; mp_restartgame 1 "apart of the previous" // comment not present;;;;;;;
+            bot_kick;;
+        """
+        minified = formatters.minify_cfg(get_parser().parse(test_input))
+        self.assertEqual(minified, """bind "mouse4" "+jump;+right";sv_cheats 1;mp_restartgame 1 "apart of the previous";bot_kick""")
 
 if __name__ == "__main__":
     unittest.main()
